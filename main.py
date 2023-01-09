a56461a23f09259dafea890335c721fc51b89480
Лагатский Даниил Константинович <d.lagatskiy@evotor.ru>
2023-01-09 22:56:15 +0300

    7

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from binance.spot import Spot as Client
 from binance.lib.utils import config_logging
 from configparser import ConfigParser
 from datetime import datetime, timezone
-import time
 from trade_analys import Analys
 import numpy as np
 
@@ -18,58 +17,112 @@ def bot(client):
         new_kline = client.klines("BTCUSDT", "1m", limit=2)
         if (analys.get_last_time() < new_kline[0][0]):
             analys.set(new_kline[0])
-        if (analys.falling_flat() or analys.upping_flat()):
-            position = True
-            if (analys.change_price(3) <= 0):
+        
 
-                """имитируем ПОКУПКУ и логируем данные (сейчас в консоль)"""
-                opening_time = datetime.now()
-                opening_price = float(new_kline[1][4])
-                take_profit = opening_price + analys.get_tp()
-                stop_loss = opening_price - analys.get_sl()
+        if analys.is_flat() and analys.down_trend_5m() and analys.approved_price("UP", new_kline[1]):
+            position = True # имитируем ПОКУПКУ и логируем данные (сейчас в консоль)
 
-                print('NEW POSITION UP', opening_time, opening_price, "---", "take_profit =", take_profit, "stop_loss =", stop_loss)
+            opening_time = datetime.now()
+            opening_price = float(new_kline[1][4])
+            take_profit = float(new_kline[0][4]) + analys.get_tp() #TP выставляем от значения последней закрытой свечи
+            stop_loss = float(new_kline[0][4]) - analys.get_sl() #SL выставляем от значения последней закрытой свечи
 
-                while position:
-                    new_kline = client.klines("BTCUSDT", "1m", limit=2)
-                    current_price = float(new_kline[1][4])
-                    if (analys.get_last_time() < new_kline[0][0]):
-                        analys.set(new_kline[0])
-                    if (current_price <= stop_loss):
-                        amoumt-=(opening_price - current_price)
-                        print('CLOSE', opening_time, "  -", opening_price - current_price, "=", amoumt)
-                        position = False
-                        break
-                    elif (current_price >= take_profit):
-                        amoumt+=(current_price - opening_price)
-                        print('CLOSE', opening_time, "  +", current_price - opening_price, "=", amoumt)
-                        position = False
-                        break
+            print('NEW POSITION UP (5m)', opening_time, opening_price, "---", "take_profit =", take_profit, "stop_loss =", stop_loss)
+
+            while position:
+                new_kline = client.klines("BTCUSDT", "1m", limit=2)
+                current_price = float(new_kline[1][4])
+                if analys.get_last_time() < new_kline[0][0]:
+                    analys.set(new_kline[0])
+                if current_price <= stop_loss:
+                    amoumt-=(opening_price - current_price)
+                    print('CLOSE', datetime.now(), "  -", opening_price - current_price, "=", amoumt)
+                    position = False
+                    break
+                elif current_price >= take_profit:
+                    amoumt+=(current_price - opening_price)
+                    print('CLOSE', datetime.now(), "  +", current_price - opening_price, "=", amoumt)
+                    position = False
+                    break
+        
+        elif analys.is_flat() and analys.up_trend_5m() and analys.approved_price("DOWN", new_kline[1]):
+            position = True # имитируем ПРОДАЖУ и логируем данные (сейчас в консоль)
+
+            opening_time = datetime.now()
+            opening_price = float(new_kline[1][4])
+            take_profit = float(new_kline[0][4]) - analys.get_tp() #TP выставляем от значения последней закрытой свечи
+            stop_loss = float(new_kline[0][4]) + analys.get_sl() #SL выставляем от значения последней закрытой свечи
+
+            print('NEW POSITION DOWN (5m)', opening_time, opening_price, "---", "take_profit =", take_profit, "stop_loss =", stop_loss)
             
-            else:
-                """имитируем ПРОДАЖУ и логируем данные (сейчас в консоль)"""
-                opening_time = datetime.now()
-                opening_price = float(new_kline[1][4])
-                take_profit = opening_price - analys.get_tp()
-                stop_loss = opening_price + analys.get_sl()
+            while position:
+                new_kline = client.klines("BTCUSDT", "1m", limit=2)
+                current_price = float(new_kline[1][4])
+                if analys.get_last_time() < new_kline[0][0]:
+                    analys.set(new_kline[0])
+                if current_price >= stop_loss:
+                    amoumt-=(current_price - opening_price)
+                    print('CLOSE', datetime.now(), "  -", current_price - opening_price, "=", amoumt)
+                    position = False
+                    break
+                elif current_price <= take_profit:
+                    amoumt+=(opening_price - current_price)
+                    print('CLOSE', datetime.now(), "  +", opening_price - current_price, "=", amoumt)
+                    position = False
+                    break
+        
+        elif analys.is_flat() and analys.down_trend_3m() and analys.approved_price("UP", new_kline[1]):
+            position = True # имитируем ПОКУПКУ и логируем данные (сейчас в консоль)
 
-                print('NEW POSITION DOWN', opening_time, opening_price, "---", "take_profit =", take_profit, "stop_loss =", stop_loss)
-                
-                while position:
-                    new_kline = client.klines("BTCUSDT", "1m", limit=2)
-                    current_price = float(new_kline[1][4])
-                    if (analys.get_last_time() < new_kline[0][0]):
-                        analys.set(new_kline[0])
-                    if (current_price >= stop_loss):
-                        amoumt-=(current_price - opening_price)
-                        print('CLOSE', opening_time, "  -", current_price - opening_price, "=", amoumt)
-                        position = False
-                        break
-                    elif (current_price <= take_profit):
-                        amoumt+=(opening_price - current_price)
-                        print('CLOSE', opening_time, "  +", opening_price - current_price, "=", amoumt)
-                        position = False
-                        break
+            opening_time = datetime.now()
+            opening_price = float(new_kline[1][4])
+            take_profit = float(new_kline[0][4]) + (analys.get_tp()/2) #TP выставляем от значения последней закрытой свечи
+            stop_loss = float(new_kline[0][4]) - analys.get_sl() #SL выставляем от значения последней закрытой свечи
+
+            print('NEW POSITION UP (3m)', opening_time, opening_price, "---", "take_profit =", take_profit, "stop_loss =", stop_loss)
+
+            while position:
+                new_kline = client.klines("BTCUSDT", "1m", limit=2)
+                current_price = float(new_kline[1][4])
+                if analys.get_last_time() < new_kline[0][0]:
+                    analys.set(new_kline[0])
+                if current_price <= stop_loss:
+                    amoumt-=(opening_price - current_price)
+                    print('CLOSE', datetime.now(), "  -", opening_price - current_price, "=", amoumt)
+                    position = False
+                    break
+                elif current_price >= take_profit:
+                    amoumt+=(current_price - opening_price)
+                    print('CLOSE', datetime.now(), "  +", current_price - opening_price, "=", amoumt)
+                    position = False
+                    break
+        
+        elif analys.is_flat() and analys.up_trend_3m() and analys.approved_price("DOWN", new_kline[1]):
+            position = True # имитируем ПРОДАЖУ и логируем данные (сейчас в консоль)
+
+            opening_time = datetime.now()
+            opening_price = float(new_kline[1][4])
+            take_profit = float(new_kline[0][4]) - (analys.get_tp()/2) #TP выставляем от значения последней закрытой свечи
+            stop_loss = float(new_kline[0][4]) + analys.get_sl() #SL выставляем от значения последней закрытой свечи
+
+            print('NEW POSITION DOWN (3m)', opening_time, opening_price, "---", "take_profit =", take_profit, "stop_loss =", stop_loss)
+            
+            while position:
+                new_kline = client.klines("BTCUSDT", "1m", limit=2)
+                current_price = float(new_kline[1][4])
+                if analys.get_last_time() < new_kline[0][0]:
+                    analys.set(new_kline[0])
+                if current_price >= stop_loss:
+                    amoumt-=(current_price - opening_price)
+                    print('CLOSE', datetime.now(), "  -", current_price - opening_price, "=", amoumt)
+                    position = False
+                    break
+                elif current_price <= take_profit:
+                    amoumt+=(opening_price - current_price)
+                    print('CLOSE', datetime.now(), "  +", opening_price - current_price, "=", amoumt)
+                    position = False
+                    break
+            
       
 
 
--- trade_analys.py
+++ trade_analys.py
@@ -16,10 +16,10 @@ class Analys:
         self.iteration += 1
         self.kline_data.append(new_kline)
         self.vol_kline.append(float(new_kline[5]))
-        if (len(self.vol_kline) > 5):
+        if len(self.vol_kline) > 5:
             self.vol_kline.pop(0)
 
-        if (sum(self.vol_kline)/5 < 200):
+        if sum(self.vol_kline)/5 < 200:
             self.flat = 1.1
             self.tp = 5
             self.sl = 2.5
@@ -34,15 +34,15 @@ class Analys:
             self.iteration += 1
             self.kline_data.append(klines_data[i])
             self.vol_kline.append(float(klines_data[i][5]))
-            if (len(self.vol_kline) > 5):
+            if len(self.vol_kline) > 5:
                 self.vol_kline.pop(0)
-            if (sum(self.vol_kline)/5 < 200):
+            if sum(self.vol_kline)/5 < 200:
                 self.flat = 1.1
                 self.tp = 5
                 self.sl = 2.5
             else:
                 self.flat = 2
-                self.tp = 5
+                self.tp = 8
                 self.sl = 4
             i += 1
 
@@ -66,17 +66,56 @@ class Analys:
     
     def falling_flat(self):
         kline_size = float(self.kline_data[self.iteration][1]) - float(self.kline_data[self.iteration][4])
-        if (-1*self.flat <= kline_size <= 0):
+        if -1*self.flat <= kline_size <= 0:
             return True
         else:
             return False
     
     def upping_flat(self):
         kline_size = float(self.kline_data[self.iteration][1]) - float(self.kline_data[self.iteration][4])
-        if (0.01 <= kline_size <= self.flat):
+        if 0.01 <= kline_size <= self.flat:
+            return True
+        else:
+            return False
+
+    def is_flat(self):
+        kline_size = float(self.kline_data[self.iteration][1]) - float(self.kline_data[self.iteration][4])
+        if 0 <= abs(kline_size) <= self.flat:
             return True
         else:
             return False
     
-    def change_price(self, period: int):
-        return float(self.kline_data[self.iteration][4]) - float(self.kline_data[self.iteration-period][1])
+    def down_trend_3m(self):
+        size_change = float(self.kline_data[self.iteration][4]) - float(self.kline_data[self.iteration-3][1])
+        if size_change >= 0 or abs(size_change) < self.tp:
+            return False
+        return True
+    
+    def down_trend_5m(self):
+        size_change = float(self.kline_data[self.iteration][4]) - float(self.kline_data[self.iteration-5][1])
+        if size_change >= 0 or abs(size_change) < self.tp*2:
+            return False
+        return True
+
+    def up_trend_3m(self):
+        size_change = float(self.kline_data[self.iteration][4]) - float(self.kline_data[self.iteration-3][1])
+        if size_change < 0 or size_change < self.tp:
+            return False 
+        return True
+
+    def up_trend_5m(self):
+        size_change = float(self.kline_data[self.iteration][4]) - float(self.kline_data[self.iteration-5][1])
+        if size_change < 0 or size_change < self.tp*2:
+            return False
+        return True
+
+    def approved_price(self, deal_direction: str, new_kline):
+        if deal_direction == 'UP' or deal_direction == 'DOWN':
+            if float(new_kline[4]) >= float(self.kline_data[self.iteration][4]) + 0.33 * self.flat:
+                return False
+            elif float(new_kline[4]) <= float(self.kline_data[self.iteration][4]) - 0.33 * self.flat:
+                return False
+            return True
+        return False
+        
+
